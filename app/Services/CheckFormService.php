806cf84f1aa97a9f27f9f8ddb3eaa4aceea522ed
NEMURENAIYORU <lhslhs301@gmail.com>
2023-12-06 11:39:49 +0900

    ✨ feat : スペリング修正

--- app/Services/CheckFormService.php
+++ app/Services/CheckFormService.php
@@ -5,7 +5,8 @@ namespace App\Services;
 class CheckFormService {
 
   public static function checkGender($request) {
-    if($request->gender === 0 ) {
+    // if($request == null) {dd($request);}
+    if($request->gender == 0 ) {
       $gender = '남성';
     } else {
       $gender = '여성';
@@ -13,7 +14,7 @@ class CheckFormService {
     return $gender;
   }
 
-  public static function chckAge($contact) {
+  public static function checkAge($contact) {
     if($contact->age === 1) { $age = '19세 미만'; }
     if($contact->age === 2) { $age = '20〜29세'; }
     if($contact->age === 3) { $age = '30〜39세'; }
