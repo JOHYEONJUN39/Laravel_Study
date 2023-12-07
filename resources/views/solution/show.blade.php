138d600d6c64bd7cfd7e614e2af38e248ebc7e6c
NEMURENAIYORU <lhslhs301@gmail.com>
2023-12-07 20:08:36 +0900

    ✨ feat : 選び取ったコメントだけ表示されるようにレイアウトを大幅に変更

--- resources/views/solution/show.blade.php
+++ resources/views/solution/show.blade.php
@@ -10,29 +10,39 @@
         <div class="max-w-4xl mx-auto bg-white shadow-md overflow-hidden sm:rounded-lg">
             <div class="p-8 space-y-8">
                 <div class="pb-8 border-b border-gray-200 mb-8">
-                    <h2 class="text-2xl font-bold">{{ $contact->title }}</h2>
-                    <p class="mt-2 text-sm text-gray-500">{{ $contact->name }}</p>
-                    <div class="mt-4 text-sm text-gray-500">
-                        <p>이메일: {{ $contact->email }}</p>
-                        @if($contact->url)
-                        <p>참고 페이지: {{ $contact->url }}</p>
-                        @endif
-                        <p>성별: {{ $gender }}</p>
-                        <p>연령: {{ $age }}</p>
-                    </div>
-                    <div class="mt-4">
-                        <p class="text-gray-700">{{ $contact->contact }}</p>
+                    <div class="mb-4 rounded-lg p-4 border border-gray-200 bg-blue-100">
+                      <h2 class="text-2xl font-bold">{{ $contact->title }}</h2>
+                      <p class="text-sm text-blue-900 font-extrabold">유저명: {{ $contact->name }}</p>
+                      <p class="text-sm text-blue-900 font-extrabold">이메일: {{ $contact->email }}</p>
+                      @if($contact->url)
+                        <p class="text-sm text-blue-900 font-extrabold">참고 페이지: {{ $contact->url }}</p>
+                      @endif
+                      <p class="text-sm text-blue-900 font-extrabold">성별: {{ $gender }}</p>
+                      <p class="text-sm text-blue-900 font-extrabold">연령: {{ $age }}</p>
+                      <p class="text-sm text-blue-900 font-extrabold">사용언어: {{ $contact->language }}</p>
+                      <p class="text-lg text-blue-900">{{ $contact->contact }}</p>
+                      <div class="flex justify-between items-end">
+                          <p class="text-s">{{ $comment->created_at->format('Y-m-d H:i') }}</p>
+                      </div>
                     </div>
                 </div>
-                <div class="pb-8 border-b border-gray-200 mb-8">
-                  <h2 class="text-2xl font-bold">해결 방법</h2>
-                  <div class="mt-4">
-                    <p class="text-gray-700">{{ $contact->solutionContact }}</p>
+
+                <div class="mb-4 rounded-lg p-4 border border-gray-200 bg-blue-100">
+                  <h2 class="text-2xl font-bold">작성자가 채택한 솔루션</h2>
+                  <p class="text-xl text-blue-900 font-extrabold">유저명: {{ $commenter->name }}</p>
+                  <p class="text-lg text-blue-900">{{ $comment->content }}</p>
+                  <div class="flex justify-between items-end">
+                      <p class="text-s">{{ $comment->created_at->format('Y-m-d H:i') }}</p>
                   </div>
                 </div>
+
                 <div class="pt-4">
                   @if(auth()->user()->email == $contact->email)
                   <a href="#" data-id="{{ $contact->id }}" onClick="deletePost(this)" class="text-red-600 hover:text-red-800">삭제</a>
+                  <form id="delete_{{ $contact->id }}" action="{{ route('solution.destroy', $contact->id) }}" method="POST" style="display: none;">
+                    @csrf
+                    @method('DELETE')
+                  </form>
                   @endif
                 </div>
             </div>
